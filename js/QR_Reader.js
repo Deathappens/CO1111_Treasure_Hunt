2bb2a7a9d6aca993d542d7f0b6cc9aceaec49df5
Deathappens <148976634+Deathappens@users.noreply.github.com>
2024-03-28 19:12:05 +0200

    Tests completed

--- js/QR_Reader.js
+++ js/QR_Reader.js
@@ -13,7 +13,7 @@ var options = {
 var scanner = new Instascan.Scanner(options);
 var scancontent = document.getElementById("scanned_content");
 var active_cam = 0;
-
+var camerastorage;
 function start_scan() {
     let qrbutton=document.getElementById("qrstartbutton");
     let popupwindow = document.getElementById("popup");
@@ -23,10 +23,8 @@ function start_scan() {
     Instascan.Camera.getCameras().then(function (cameras) {
         if (cameras.length > 0) {
             scanner.start(cameras[active_cam]);
-            console.log("I have detected"+cameras.length+" cameras");
-
-            sessionStorage.camerastorage = cameras;
-            console.log("I have detected"+cameras.length+" cameras");
+            console.log("I have detected "+cameras.length+" cameras");
+             camerastorage = cameras;
         } else {
             console.log('No cameras found.');
             alert("No cameras found.");
@@ -45,7 +43,7 @@ function start_scan() {
 function stop_scan() {
     let popupwindow = document.getElementById("popup");
     let qrbutton=document.getElementById("qrstartbutton");
-    scanner.stop(active_cam);
+    scanner.stop(active_cam); //to be perfectly honest, I'm not sure why this works, but...it works.
     popupwindow.style.display = "none";
     qrbutton.style.display="flex";
 
@@ -53,9 +51,8 @@ function stop_scan() {
 
 function camera_cycle() {
     scanner.stop(active_cam);
-    console.log(sessionStorage.camerastorage);
-    console.log("Session storage has saved"+sessionStorage.camerastorage.length+"cameras");
-    if (active_cam < sessionStorage.camerastorage.length) {
+    console.log(camerastorage);
+    if (active_cam < camerastorage.length) {
         active_cam++;
         console.log("currently active cam is camera number"+active_cam);
     } else {
